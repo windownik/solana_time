f38b486346dea8475bc8399e9f7219537529a442
Miikita Mislivec <windownik@gmail.com>
2025-11-21 17:33:47 +0300

    add GetIt to project

---
+++ lib/domain/repository/api.dart
@@ -0,0 +1,38 @@
+
+
+
+import 'dart:async';
+
+
+class MainApi {
+  StreamController<DateTime>? _controller;
+  Timer? _timer;
+
+  Stream<DateTime> startLocalStream() {
+    if (_controller != null) {
+      return _controller!.stream;
+    }
+    _controller = StreamController<DateTime>.broadcast();
+    _timer = Timer.periodic(Duration(seconds: 1), (_) {
+      if (_controller == null) return;
+
+      if (_controller!.isClosed) return;
+
+      _controller?.add(DateTime.now());
+
+    });
+    return _controller!.stream;
+  }
+
+  void closeLocalStream() {
+    _timer?.cancel();
+    _controller?.close();
+    _controller = null;
+    _timer = null;
+  }
+
+
+  void dispose() {
+    closeLocalStream();
+  }
+}
