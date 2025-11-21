f23804750d072273104f9c5eb336b489bce38d4a
Miikita Mislivec <windownik@gmail.com>
2025-11-21 12:20:32 +0300

    add init and copyWith to main state

--- lib/presentation/bloc/main_state.dart
+++ lib/presentation/bloc/main_state.dart
@@ -1,15 +1,35 @@
 import 'package:equatable/equatable.dart';
 
+import 'events/main_event.dart';
+
 class MainState extends Equatable {
-  final MainState mainState;
+  final MainEvents timeEvent;
+  final MainEvents networkEvent;
+  final MainEvents runEvent;
 
   const MainState._({
-    required this.mainState,
+    required this.timeEvent,
+    required this.networkEvent,
+    required this.runEvent,
 });
+  factory MainState.init() => MainState._(
+      timeEvent: SolanaTime(),
+      networkEvent: Disconnected(),
+      runEvent: StopEvent());
 
+  MainState copyWith(
+      MainEvents? timeEvent,
+      MainEvents? networkEvent,
+      MainEvents? runEvent,
+      ) =>
+      MainState._(
+      timeEvent: timeEvent ?? this.timeEvent,
+      networkEvent: networkEvent ?? this.timeEvent,
+      runEvent: runEvent ?? this.timeEvent
+      );
 
 
   @override
-  List<Object?> get props => [mainState];
+  List<Object?> get props => [timeEvent, networkEvent, runEvent];
 
 }
