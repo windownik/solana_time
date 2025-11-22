d5b0c28966ef8bcc52e8decc894a5838bb3f2e3e
Miikita Mislivec <windownik@gmail.com>
2025-11-22 11:59:45 +0300

    create LocalTimeApi and move logic to local_time_source.dart

--- lib/domain/repository/api.dart
+++ lib/domain/repository/api.dart
@@ -3,34 +3,21 @@
 
 import 'dart:async';
 
+import '../../data/sources/local_time_source.dart';
 import '../../data/sources/solana_time_source.dart';
 
 
 class MainApi {
-  StreamController<DateTime>? _localTimeController;
-  Timer? _timer;
   SolanaApi solanaApi = SolanaApi();
+  LocalTimeApi localTimeApi = LocalTimeApi();
+
 
   Stream<DateTime> startLocalStream() {
-    if (_localTimeController != null) {
-      return _localTimeController!.stream;
-    }
-    _localTimeController = StreamController<DateTime>.broadcast();
-    _timer = Timer.periodic(Duration(seconds: 1), (_) {
-      if (_localTimeController == null) return;
-
-      if (_localTimeController!.isClosed) return;
-      _localTimeController?.add(DateTime.now());
-
-    });
-    return _localTimeController!.stream;
+    return localTimeApi.startLocalStream();
   }
 
   void closeLocalStream() {
-    _timer?.cancel();
-    _localTimeController?.close();
-    _localTimeController = null;
-    _timer = null;
+    localTimeApi.cancel();
   }
 
 
@@ -46,7 +33,6 @@ class MainApi {
 
 
   void dispose() {
-    solanaApi.cancel();
     closeLocalStream();
     closeSolanaStream();
   }
